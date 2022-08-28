6890d5844b9b4fd90483d4ce929f9311e73b47f2
Digest Labs <info@digestlabs.io>
2022-08-28 20:40:27 +0200

    Enforcing https

--- _next/static/zPBQkXPBshDyl_OommlZ3/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/185-d79808d018a8cd761348.js","static/chunks/pages/index-4f1c7078079f62e8fb13.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
